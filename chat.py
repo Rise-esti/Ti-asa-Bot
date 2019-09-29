615bc0ba5d53a41433c8ba94ea589dcef0808c1e
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2019-09-29 23:48:07 +0300

    nouveau adapteur

--- chat.py
+++ chat.py
@@ -1,3 +1,4 @@
+import chatterbot
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 
@@ -5,7 +6,18 @@ from chatterbot.trainers import ListTrainer
 chatbot = ChatBot(
     "@ti-asa",
     database_uri='sqlite:///db/ti-asa.db',
-    logic_adapters=["chatterbot.logic.BestMatch"],
+    logic_adapters=[
+        {
+            'import_path': "chatterbot.logic.BestMatch",
+            'default_response': 'Je ne comprends ce que vous dites',
+            'maximum_similarity_threshold':  0.90
+        },
+
+        {
+            'import_path': "chatterbot.logic.MathematicalEvaluation",
+            'language': chatterbot.languages.FR
+         }
+    ],
 )
 
 trainer = ListTrainer(chatbot)
